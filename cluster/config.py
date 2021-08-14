f4ac45819621f79eb523a3e8fa087ded52832e5a
Jaironlanda <jaironlanda@yahoo.com>
2021-08-14 10:42:03 +0800

    clean up config.py

--- cluster/config.py
+++ cluster/config.py
@@ -18,22 +18,22 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    DB_NAME = "development-db"
-    DB_USERNAME = "admin"
-    DB_PASSWORD = "example"
-
-    IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
-   
     SESSION_COOKIE_SECURE = False
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # DB_NAME = "development-db"
+    # DB_USERNAME = "admin"
+    # DB_PASSWORD = "example"
+
+    # IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
+   
 
 class TestingConfig(Config):
     TESTING = True
-
-    DB_NAME = "testing-db"
-    DB_USERNAME = "admin"
-    DB_PASSWORD = "example"
-
     SESSION_COOKIE_SECURE = False
+    
+    # DB_NAME = "testing-db"
+    # DB_USERNAME = "admin"
+    # DB_PASSWORD = "example"
